4a7660cea09185888f9f363e023ea5a838988607
grJDh <rusmukhametov@gmail.com>
2022-06-07 18:53:23 +0300

    Imported normalize.css to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+import './normalize.css';
 import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
