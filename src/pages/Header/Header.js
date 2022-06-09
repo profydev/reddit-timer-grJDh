8dd674edc1ccab1dc0dec4da340a208a1ada23af
grJDh <rusmukhametov@gmail.com>
2022-06-09 16:55:48 +0300

    Added unstyled header

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import NavLink from '../../components/NavLink/NavLink';
+
+import logo from './logo.png';
 
 // eslint-disable-next-line arrow-body-style
 const Header = () => {
+  const HeaderWrapper = styled.header`
+    margin: 1em;
+  `;
+
+  const Nav = styled.nav`
+    margin: 1em;
+  `;
+
+  const Logo = styled.img`
+    margin: 1em;
+  `;
+
   return (
-    <p>Header</p>
+    <HeaderWrapper>
+      <Logo
+        src={logo}
+        alt="Reddit Timer Logo"
+      />
+
+      <Nav>
+        <NavLink text="Search" href="#" />
+        <NavLink text="How it works" href="#" />
+        <NavLink text="About" href="#" />
+      </Nav>
+    </HeaderWrapper>
   );
 };
 
