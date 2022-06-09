854da6e503d76a607fc67bc385f70de303162cbd
grJDh <rusmukhametov@gmail.com>
2022-06-09 13:02:57 +0300

    Added basic theme and rewrote global styles
    - moved Normalize from index.js to App
    - moved global styles from index.css to GlobalStyle.js
    - rewrote global styles with createGlobalStyle
    - moved font-face from index.html to GlobalStyle.js
    - added ThemeProvider with a theme to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
+import { Normalize } from 'styled-normalize';
+import theme from './theme';
+import GlobalStyle from './GlobalStyle';
+
 import Home from './pages/Home/Home';
 import Search from './pages/Search/Search';
 import NotFound from './pages/NotFound/NotFound';
@@ -9,7 +13,9 @@ import NotFound from './pages/NotFound/NotFound';
 // eslint-disable-next-line arrow-body-style
 const App = () => {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <GlobalStyle />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Normalize } from 'styled-normalize';
 import App from './App';
 
-// import './normalize.css';
-import './index.css';
-
-const Root = () => (
-  <>
-    <Normalize />
-    <App />
-  </>
+// eslint-disable-next-line react/jsx-filename-extension
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
 );
-
-ReactDOM.render(<Root />, document.querySelector('#root'));
